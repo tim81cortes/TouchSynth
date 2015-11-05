36d44df7e15e1cc2d9330f5c8189483f2320c068
Tim Aylott <T_Aylott@138-38-242-13.eduroam.bath.ac.uk>
2015-11-05 10:23:24 +0000

    First commit

---
+++ gen/uk/org/mangotsfieldschool/t/aylott/butonsound/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package uk.org.mangotsfieldschool.t.aylott.butonsound;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
